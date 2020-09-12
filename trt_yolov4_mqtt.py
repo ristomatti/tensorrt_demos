3cb9e855d214c67fd3026363628ccbd6dee9fe01
Ristomatti Airo <ristomatti.airo@gmail.com>
2020-09-12 11:04:17 +0300

    Refactor to support multiple inputs

--- trt_yolov4_mqtt.py
+++ trt_yolov4_mqtt.py
@@ -12,7 +12,7 @@ from utils.yolo_with_plugins import TrtYOLO
 from utils.visualization import BBoxVisualization
 
 MQTT_SERVER = '192.168.5.20'
-MQTT_DETECT_TOPIC = 'trt_yolo/detect/+'
+MQTT_DETECT_TOPIC = 'trt_yolo/detect/+/+'
 MQTT_RESULT_TOPIC_BASE = 'trt_yolo/result'
 
 INPUT_HW = (288, 288)
@@ -34,29 +34,37 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
-class TrtThread(threading.Thread):
+class TrtThread(threading.Thread, threading.Condition):
 
-    def __init__(self, model, conf_th):
+    def __init__(self, model, condition, client, conf_th):
         threading.Thread.__init__(self)
         self.model = model
         self.conf_th = conf_th
+        self.condition = condition
+        self.client = client
         self.trt_yolo = None   # to be created when run
         self.cuda_ctx = None  # to be created when run
-        self.running = False
 
     def run(self):
         global image
+        global th_abort
 
         print('TrtThread: loading the TRT YOLO engine...')
         self.cuda_ctx = cuda.Device(0).make_context()  # GPU 0
         self.trt_yolo = TrtYOLO(self.model, INPUT_HW)
+
         print('TrtThread: start running...waiting for images to analyze')
-        self.running = True
-        while self.running:
+
+        while not th_abort:
+            with self.condition:
+                self.condition.wait()
             if image is not None:
-                #print('Found image with msg_id: ', msg_id)
-                img = image
-                image = None
+                with self.condition:
+                    img = image
+                    image = None
+                    _cam_id = cam_id
+                    _msg_id = msg_id
+                    self.condition.notify()
 
                 tic = time.time()
                 boxes, confs, clss = self.trt_yolo.detect(img, self.conf_th)
@@ -68,8 +76,8 @@ class TrtThread(threading.Thread):
                 img = vis.draw_bboxes(img, boxes, confs, clss)
 
                 # Prepare and send result via MQTT
-                classes_topic = MQTT_RESULT_TOPIC_BASE + '/' + msg_id + '/classes'
-                image_topic = MQTT_RESULT_TOPIC_BASE + '/' + msg_id + '/image'
+                classes_topic = MQTT_RESULT_TOPIC_BASE + '/' + _cam_id + '/' + _msg_id + '/classes'
+                image_topic = MQTT_RESULT_TOPIC_BASE + '/' + _cam_id + '/' + _msg_id + '/image'
 
                 class_names = list(map(lambda x: cls_dict.get(x), clss))
                 class_score_tuples = list(zip(class_names, confs))
@@ -79,86 +87,86 @@ class TrtThread(threading.Thread):
                     score = round(score, 2)
                     detections.append({'class': class_name, 'score': score})
 
-                print('Processed msg_id {0:15s} in {1:.2f}s, result: {2}'
-                        .format(msg_id, inference_time, detections))
+                print('Processed msg_id {1:15s} from {0:7s} in {2:.2f}s, result: {3}'
+                        .format(_cam_id, _msg_id, inference_time, detections))
 
                 classes_payload = json.dumps(detections, cls=NumpyEncoder)
 
                 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
                 image_payload = cv2.imencode('.jpg', img)[1].tostring()
 
-                client_thr = mqtt.Mosquitto()
-                client_thr.connect(MQTT_SERVER, 1883, 60)
-                client_thr.loop_start()
+                self.client.publish(image_topic, image_payload, 1, False)
+                self.client.publish(classes_topic, classes_payload, 1, False)
 
-                client_thr.publish(image_topic, image_payload, 1, False)
-                client_thr.publish(classes_topic, classes_payload, 1, False)
-
-                client_thr.loop_stop()
-                client_thr.disconnect()
-        del self.trt_yolo
         self.cuda_ctx.pop()
+        del self.trt_yolo
         del self.cuda_ctx
         print('TrtThread: stopped...')
+        exit()
 
-    def stop(self):
-        self.running = False
-        self.join()
 
-def initmqtt(mqttThreadEvent):
+class MqttThread(threading.Thread, threading.Condition):
 
-    def on_connect(client, userdata, flags, rc):
+    def __init__(self, condition, client):
+        threading.Thread.__init__(self)
+        self.condition = condition
+        self.client = client
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
+        resp = self.client.connect(MQTT_SERVER, 1883, 60)
+        self.client.loop_start()
+
+    def on_connect(self, client, userdata, flags, rc):
         print("Connected to broker:", rc)
         client.subscribe(MQTT_DETECT_TOPIC, 0)
 
-    def on_subscribe(client, userdata, mid, granted_qos):
+    def on_subscribe(self, client, userdata, mid, granted_qos):
         print('Subscribed to topics:', userdata, mid, granted_qos)
 
-
-    def on_message(client, userdata, msg):
+    def on_message(self, client, userdata, msg):
+        #print('got msg ' + msg.topic)
         global image
         global msg_id
+        global cam_id
         global th_abort
-        #print (msg.payload)
-        #print (len(msg.payload))
-        if(len(msg.payload)>1000):
-            msg_id = msg.topic.split('/')[2]
-            # convert string of image data to uint8
-            nparr = np.fromstring(msg.payload, np.uint8)
-            # decode image
-            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        if (len(msg.payload) > 1000):
+            with self.condition:
+                # get id's from topic
+                cam_id = msg.topic.split('/')[2]
+                msg_id = msg.topic.split('/')[3]
+                # convert string of image data to uint8
+                nparr = np.fromstring(msg.payload, np.uint8)
+                # decode image
+                image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                self.condition.notify()
         if msg.payload.decode('utf-8') == 'stop':
             th_abort = True
 
-    client = mqtt.Mosquitto()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.on_subscribe = on_subscribe
-    resp = client.connect(MQTT_SERVER, 1883, 60)
-    client.loop_start()
-    while th_abort == False:
-        time.sleep(1)
-    client.loop_stop()
-    client.disconnect()
-    print ("mqttThread terminated")
-    exit()
+    def run(self):
+        global th_abort
+        while not th_abort:
+            time.sleep(1)
+        self.client.loop_stop()
+        self.client.disconnect()
+        print ("MqttThread: stopped...")
+        with self.condition:
+            self.condition.notifyAll()
+        exit()
 
-image = None
+
 th_abort = False
+image = None
 msg_id = None
-
-mqttThreadEvent = threading.Event()
-mqttThread = threading.Thread(
-        target=initmqtt,
-        args=(mqttThreadEvent,)
-        )
-mqttThread.start()
+cam_id = None
 
 cuda.init()  # init pycuda driver
+client = mqtt.Mosquitto()
 
-trt_thread = TrtThread(YOLOV4_MODEL, conf_th=CONFIDENCE_THRESHOLD)
-trt_thread.start()  # start the child thread
-while th_abort == False:
-    time.sleep(1)
-trt_thread.stop()   # stop the child thread
+condition = threading.Condition()
+mqtt_thread = MqttThread(condition, client,)
+mqtt_thread.start()
+trt_thread = TrtThread(YOLOV4_MODEL, condition, client, CONFIDENCE_THRESHOLD,)
+trt_thread.start()
 
